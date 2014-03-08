da07499768405737c8c74a622b3d61a16e738806
kimrin <kittykimura@gmail.com>
2014-03-08 21:40:11 +0900

    remove gen bin.

--- StopOverSleptMultiScreens/gen/org/mechajyo/stopoversleptmultiscreens/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package org.mechajyo.stopoversleptmultiscreens;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
